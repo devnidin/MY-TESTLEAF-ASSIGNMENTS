efd7bd4d8f666c005a9daf47665d0f70793e9df8
NIDIN DEV <65495191+devnidin@users.noreply.github.com>
2022-07-25 20:22:14 +0530

    Add files via upload

---
+++ myprojectday5/Test.java
@@ -0,0 +1,19 @@
+package myprojectday5;
+
+public class Test {
+ public void printName(String name) {
+	 System.out.println(name);
+ }
+ 
+ public void add(int a, int b) {
+	int c=a+b;
+	 System.out.println(c);
+ }
+ 
+	public static void main(String[] args) {
+Test test=new Test();
+test.add(25, 10);
+
+	}
+
+}
